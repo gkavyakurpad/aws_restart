1de08bc00abdf03ecfaa4f1c72853fcb9ba45783
gkavyakurpad <kurpad.kavya@gmail.com>
2024-01-16 12:42:40 +0530

    Python Basic

---
+++ collections.py
@@ -0,0 +1,11 @@
+"""
+Your module description
+"""
+myFruitList = ["apple", "banana", "cherry"]
+print(myFruitList)
+print(type(myFruitList))
+print(myFruitList[0])
+print(myFruitList[1])
+print(myFruitList[2])
+myFruitList[2] = "orange"
+print(myFruitList)
